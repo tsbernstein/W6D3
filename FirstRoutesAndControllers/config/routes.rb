f40a3bd40cfe447f5c8444e52e926c06b59d8f26
Tyler Bernstein <tsbernstein2593@gmail.com>
2021-05-26 13:05:00 -0500

    first route and first controller

--- FirstRoutesAndControllers/config/routes.rb
+++ FirstRoutesAndControllers/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users
+  get '/users', to: 'users#index'
+  
+  get '/users/:id', to: 'users#show'
+
+  post '/users/:id', to: 'users#create'
+
+  patch '/users/:id', to: 'users#update'
+
+  delete '/users/:id', to: 'users#delete'
+
 end
