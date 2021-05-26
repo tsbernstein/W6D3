80c3838850bb2541468e0625103ba4eeae013855
taihuynh812 <taihuynh@revlisoft.com>
2021-05-26 15:50:33 -0400

    completed create action

--- FirstRoutesAndControllers/app/controllers/users_controller.rb
+++ FirstRoutesAndControllers/app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action"
+        @users = User.all
+        render json: @users
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:email, :name))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
--- FirstRoutesAndControllers/db/seeds.rb
+++ FirstRoutesAndControllers/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+User.create!(email:'tyler@app.com', name: 'Tyler')
+User.create!(email:'tai@app.com', name: 'Tai')
+User.create!(email:'brad@app.com', name: 'Brad')
+User.create!(email:'cathy@app.com', name: 'Cathy')
+User.create!(email:'justin@app.com', name: 'Justin')
